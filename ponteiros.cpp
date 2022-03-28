edebe8d87a90d205333fc085bc6456487e66c98f
edson-silva <81913175+edson-silva@users.noreply.github.com>
2022-03-28 03:30:25 -0300

    Add files via upload

---
+++ ponteiros.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int *p;
+int vetor[10];
+
+p=&vetor[0];
+
+cout << "\n" << p << "\n";
+
+p=&vetor[1];
+cout << "\n" << p << "\n";
+
+return 0;
+
+}
